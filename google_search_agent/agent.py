0f845aa0055bf8c092cd44a3039fa61bf9e3edb0
Akash Rachewad <Akash.Rachewad@wendys.com>
2026-01-05 16:27:35 -0500

    commit_gadk

---
+++ google_search_agent/agent.py
@@ -0,0 +1,16 @@
+from google.adk.agents import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import google_search
+from google.genai import types
+from .retry import retry_config
+
+root_agent = Agent(
+    name="helpful_assistant",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    instruction="You are a helpful assistant. Use Google Search for current info or if unsure.",
+    tools=[google_search],
+)
